aac90506f8ece1ba943ca5f2b2dc25a9fd914470
qianwen_pan <qianwen.pan2021@gmail.com>
2023-07-17 12:45:42 -0400

    Update component: Modal and import Modal in PlaceItem use as popup window to show map

--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -1,21 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
+import Backdrop from "./Backdrop";
 import "./Modal.css";
 
-function ModalOverlay(props){
-    const content = (
-        <div className={`modal ${props.className}`} style={props.style}>
-            <header className={`modal__header ${props.headerClass}`}>
-                <h2>{props.header}</h2>
-            </header>
 
+function ModalOverlay(props) {
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
         </div>
-    );
-    return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
 }
 
-function Modal(){
+function Modal() {
 
 }
 export default Modal;
