86ff5e3ebad6da89d1cfd656098fd809133db1e8
qianwen_pan <qianwen.pan2021@gmail.com>
2023-07-20 18:04:22 -0400

    Add component: Google map

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import "./Map.css";
+function Map(props) {
+  const mapRef = useRef();
+  useEffect(() => {
+    const position = { lat: -25.344, lng: 131.031 };
 
-function Map(props){
-    return <div className={`map ${props.className}`} style={props.style}></div>
+    // The map, centered at Uluru
+    const map = new window.google.maps.Map(mapRef.current, {
+      zoom: 4,
+      center: position,
+    });
+
+    // The marker, positioned at Uluru
+    new window.google.maps.Marker({
+      map: map,
+      position: position,
+    });
+  }, []);
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
 }
 export default Map;
