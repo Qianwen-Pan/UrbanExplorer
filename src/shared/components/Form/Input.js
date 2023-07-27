c3cee5444f0a0e700c62771943aaa02cb8f352f3
qianwen_pan <qianwen.pan2021@gmail.com>
2023-07-27 16:14:17 -0400

    Add input validation using useReducer hook

--- src/shared/components/Form/Input.js
+++ src/shared/components/Form/Input.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./Input.css";
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: false,
+      };
+    default:
+      return state;
+  }
+};
 function Input(props) {
+  const initialState = { value: "", isValid: false };
+  const [state, dispatch] = useReducer(inputReducer, initialState);
+    const changeHandler = (event) => {
+
+    }
   const element =
     props.element === "input" ? (
-      <input type="text" id={props.id} placeholder={props.placeholder} />
+      <input
+        type="text"
+        id={props.id}
+        placeholder={props.placeholder}
+        onChange={(event) => dispatch({type: "CHANGE", val: event.target.value})}
+      />
     ) : (
       <textarea id={props.id} rows={props.rows || 3} />
     );
