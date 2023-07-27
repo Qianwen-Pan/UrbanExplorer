0030fe54aa1ddcdd742a4c17111e363a6c7ec49b
qianwen_pan <qianwen.pan2021@gmail.com>
2023-07-27 11:21:09 -0400

    Update Input UI component

--- src/shared/components/Form/Input.js
+++ src/shared/components/Form/Input.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 function Input(props) {
-
+  const element =
+    props.element === "input" ? (
+      <input type="text" id={props.id} placeholder={props.placeholder} />
+    ) : (
+      <textarea id={props.id} rows={props.rows || 3} />
+    );
+  return (
+    <div>
+      <label>{props.label}</label>
+      {element}
+    </div>
+  );
 }
 export default Input;
